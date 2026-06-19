4340e04ffd2d569f3b49c8ce2706b709fa8ca74c
Kimsolmin <solmin614@gmail.com>
2026-06-19 12:16:19 +0900

    first commit

---
+++ src/main/java/com/susol/susolstudy/SusolstudyApplication.java
@@ -0,0 +1,13 @@
+package com.susol.susolstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusolstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SusolstudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/susol/susolstudy/SusolstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susol.susolstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusolstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
