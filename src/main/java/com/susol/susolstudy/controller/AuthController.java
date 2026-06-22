bfbd52c234df52a1e4a2ec9ba6d1d8207a5aacb3
Kimsolmin <solmin614@gmail.com>
2026-06-22 22:39:24 +0900

    sm 로그인 유지 처리

--- src/main/java/com/susol/susolstudy/controller/AuthController.java
+++ src/main/java/com/susol/susolstudy/controller/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
-public class LoginController {
+public class AuthController {
 
     @GetMapping("/login")
     public String loginPage() {
